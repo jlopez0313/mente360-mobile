65ebeabe9608212bf9439288f7c809a9dea37783
Jonathan Lopez <jlopez0313@hotmail.com>
2025-12-02 12:17:15 -0500

    sqlite removed

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,6 @@ import "@ionic/react/css/palettes/dark.class.css";
 import "./styles.scss";
 import "./theme/variables.css";
 
-import { CapacitorSQLite, SQLiteConnection } from "@capacitor-community/sqlite";
-import { Capacitor } from "@capacitor/core";
-import { JeepSqlite } from "jeep-sqlite/dist/components/jeep-sqlite";
 
 
 const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
@@ -34,22 +31,6 @@ toggleDarkMode();
 window.addEventListener("DOMContentLoaded", async () => {
   try {
     
-    const platform = Capacitor.getPlatform();
-
-    if (platform === "web") {
-      const sqlite = new SQLiteConnection(CapacitorSQLite);
-
-      customElements.define("jeep-sqlite", JeepSqlite);
-      const jeepSqliteEl = document.createElement("jeep-sqlite");
-      document.body.appendChild(jeepSqliteEl);
-
-      await customElements.whenDefined("jeep-sqlite");
-      console.log(`after customElements.whenDefined`);
-
-      await sqlite.initWebStore();
-      console.log(`after initWebStore`);
-    }
-
     const container = document.getElementById("root");
     const root = createRoot(container!);
     root.render(
